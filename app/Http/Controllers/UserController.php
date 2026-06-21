388f10f2ab83142e871b591246e1bb3d54a48662
Stefani Parera <angelinaparera22-del.com>
2026-06-21 23:44:58 +0800

    Pertemuan 12: User Create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -13,6 +15,7 @@ class UserController extends Controller
     {
         return view('user.index', [
             'title' => 'User',
+            'users' => User::latest()->get()
         ]);
     }
 
@@ -21,7 +24,9 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('user.create', [
+            'title' => 'Tambah User',
+        ]);
     }
 
     /**
@@ -29,7 +34,41 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $validated = $request->validate([
+    'name' => 'required|string|max:255',
+    'email' => 'required|string|email|max:255|unique:users,email',
+    'password' => 'required|string|min:8',
+    'passwordconfirm' => 'required|same:password',
+    'avatar' => 'nullable|image|mimes:jpeg,png,jpg|max:1048', // jika berupa file gambar
+    'role' => 'required|in:Superadmin,Admin',
+], [
+    'name.required' => 'Nama tidak boleh kosong.',
+    'name.max' => 'Nama maksimal 255 karakter.',
+    'email.required' => 'Email tidak boleh kosong.',
+    'email.email' => 'Format email tidak valid.',
+    'email.unique' => 'Email sudah terdaftar.',
+    'password.required' => 'Password tidak boleh kosong.',
+    'password.min' => 'Password minimal harus 8 karakter.',
+    'role.required' => 'Role harus dipilih.',
+    'role.in' => 'Role harus berupa Superadmin atau Admin.',
+]);
+
+    if($request->file('avatar')) {
+        $validated['avatar'] = $request->file('avatar')->store('avatar', 'public');
+    }
+    try {
+        DB::beginTransaction();
+
+        // Simpan organisasi
+        User::create($validated);
+        DB::commit();
+    
+
+        return to_route('user.index')->withSuccess('Data Berhasil Ditambahkan');
+    } catch (\Exception $e) {
+        DB::rollBack();
+        return to_route('user.create')->withErrors('Data gagal Ditambahkan');
+    }
     }
 
     /**
